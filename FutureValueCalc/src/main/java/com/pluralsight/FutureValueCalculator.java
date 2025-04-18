a8489404ba5b2e04120bef1a815b08845db01b11
krosengr4 <rosenkev4@gmail.com>
2025-04-18 04:07:42 -0500

    Added prompts to collect user data

--- FutureValueCalc/src/main/java/com/pluralsight/FutureValueCalculator.java
+++ FutureValueCalc/src/main/java/com/pluralsight/FutureValueCalculator.java
@@ -6,7 +6,7 @@ public class FutureValueCalculator {
 
     public static void main(String[] args) {
 
-        // Create instance of scanner
+        //region Create instance of scanner
         Scanner myScanner = new Scanner(System.in);
 
         //Ask for user info and store in variables
@@ -15,6 +15,10 @@ public class FutureValueCalculator {
 
         System.out.println("What is your annual interest rate? ");
         double interestRate = myScanner.nextDouble();
+
+        System.out.println("How many years will your deposit earn interest? ");
+        double numberOfYears = myScanner.nextDouble();
+        //endregion
 
     }
 
